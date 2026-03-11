fd7b09b2fe538c66c9caf7b6d225df2d1d193be3
FooterReal <footerreal@gmail.com>
2026-03-11 09:13:04 +0100

    Added 'Hello World!' bootloader program

--- bootloader/boot.asm
+++ bootloader/boot.asm
@@ -1,7 +1,32 @@
 ; boot.asm - A simple placeholder for testing the bootloader setup
-hang:
-    jmp hang
+bits 64
+default rel
 
-    times 510-($-$$) db 0
-    db 0x55
-    db 0xAA
+section .rodata
+hello_msg:
+dw 'H','e','l','l','o',',',' ','W','o','r','l','d','!',13,10,0
+
+section .text
+global efi_main:function
+
+extern InitializeLib
+extern Print
+
+; UEFI x64 entry args:
+; rdi = ImageHandle
+; rsi = SystemTable pointer?
+efi_main:
+    sub rsp, 8 ; Align stack for external C calls
+
+    ; InitializeLib(ImageHandle, SystemTable)
+    call InitializeLib
+
+    ; Print("Hello, World!\n")
+    lea rdi, [hello_msg] ; lead string pointer
+    xor rax, rax
+    call Print
+
+    ; return EFI_SUCCESS
+    xor rax, rax ; Clear rax, EFI_SUCCESS = 0
+    add rsp, 8; Restore stack
+    ret
